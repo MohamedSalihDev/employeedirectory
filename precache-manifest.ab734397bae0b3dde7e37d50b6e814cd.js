51db54c1bf44c6b136ebfbf25ad9a31ac685c36d
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2020-10-24 08:58:23 -0700

    Updates

---
+++ precache-manifest.ab734397bae0b3dde7e37d50b6e814cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e951b5c390733b75da16b59b1c67858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "519fa9e6689b03d4a7af",
+    "url": "/static/css/main.7573948c.chunk.css"
+  },
+  {
+    "revision": "162df8096143e02e9927",
+    "url": "/static/js/2.d0dea792.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.d0dea792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519fa9e6689b03d4a7af",
+    "url": "/static/js/main.e0d52c91.chunk.js"
+  },
+  {
+    "revision": "35839d639db8e60032a1",
+    "url": "/static/js/runtime-main.f78ad96b.js"
+  }
+]);
