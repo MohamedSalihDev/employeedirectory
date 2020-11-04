90f72740bb1d2b9e80b1335582844829a4b6abdf
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2020-11-03 18:11:19 -0700

    moved build folder to src

---
+++ src/build/precache-manifest.e6721ed6d2ec4ec25c0220ee6a21046b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e83a5ca00e9489a66593aed8c842ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "519fa9e6689b03d4a7af",
+    "url": "/static/css/main.7573948c.chunk.css"
+  },
+  {
+    "revision": "162df8096143e02e9927",
+    "url": "/static/js/2.d0dea792.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.d0dea792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519fa9e6689b03d4a7af",
+    "url": "/static/js/main.e0d52c91.chunk.js"
+  },
+  {
+    "revision": "35839d639db8e60032a1",
+    "url": "/static/js/runtime-main.f78ad96b.js"
+  }
+]);
